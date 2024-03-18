b318b382b5559f68dc229ce379da9d9a4a3b42ab
jenny <jenny.huang@campus.lmu.de>
2024-03-19 00:26:42 +0100

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './globals.scss';
 import { Providers } from './providers';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Goldener Reiter',
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
       <body>
         <Providers>{children}</Providers>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
