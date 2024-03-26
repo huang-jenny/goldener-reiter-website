d43f38c29da5f33e76a18fa3b57827ed87928e3c
jenny <jenny.huang@campus.lmu.de>
2024-03-26 23:32:58 +0100

    updated led animation to only have two running texts

--- src/lib/lottie/setLedText.js
+++ src/lib/lottie/setLedText.js
@@ -1,7 +1,7 @@
 const setLedText = (ledsJson, text, lineNumber) => {
   ledsJson?.assets[lineNumber]?.layers?.forEach((layer) => {
     if (layer.t) {
-      layer.t.d.k[0].s.t = text;
+      layer.t.d.k[0].s.t = text + ' ' + text; // duplicate so it does not jump when theres few text
     }
   });
 };
